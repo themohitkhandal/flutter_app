d6691fca8e250b8ee2491349f5c8a1f724e137e9
Mohit Khandal <mohit1319khandal@gmail.com>
2023-07-19 12:41:31 +0530

    Profile items completed. next Dark Mode

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/core/constants.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -8,6 +9,30 @@ class ProfilePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Profile'),
+      ),
+      body: const Column(
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage('images/sing.png'),
+            radius: 70,
+            backgroundColor: Colors.transparent,
+          ),
+          SizedBox(
+            height: kHeight10,
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Mohit Khandal'),
+          ),
+          ListTile(
+            leading: Icon(Icons.email),
+            title: Text('mohit1319khandal@gmail.com'),
+          ),
+          ListTile(
+            leading: Icon(Icons.web),
+            title: Text('themohitkhandal.com'),
+          )
+        ],
       ),
     );
   }
